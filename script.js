fffeff755344d15f9d15092f47a16a861f611578
Alejandro Arias Alarcon <lidee6227@gmail.com>
2024-08-14 09:46:07 -0500

    modificacion a los archivos css html y javascript

--- script.js
+++ script.js
@@ -1,18 +1,53 @@
 const textArea = document.querySelector(".text_area");
 const mensaje = document.querySelector(".mensaje");
-
+const right1 = document.querySelector(".right");
+const right2 = document.querySelector(".right-2");
+const botonCopiar = document.querySelector(".button-copy");
 
 function btn_encriptar() {
-    const textoEncriptado = encriptar(textArea.value);
-    mensaje.value = textoEncriptado;
-    textArea.value = "";
-    // mensaje.style.backgroundImage = "none";
+    if (textArea.value != ""){
+        const textoEncriptado = encriptar(textArea.value);
+        mensaje.value = textoEncriptado;
+        textArea.value = "";
+        right1.style.display = "none";
+        right2.style.display = "flex";
+
+        botonCopiar.classList.remove("hidden");
+        document.querySelector(".texto-copiado").style.display ="none";
+
+    } else {
+        right1.style.display = "flex";
+        right2.style.display = "none";
+    }
+    
+    
 }
 
 function btn_desencriptar() {
-    const textoDesencriptado = desencriptar(textArea.value);
-    mensaje.value = textoDesencriptado;
-    textArea.value = "";
+    if (textArea.value != ""){
+        const textoDesencriptado = desencriptar(textArea.value);
+        mensaje.value = textoDesencriptado;
+        textArea.value = "";
+        right1.style.display = "none";
+        right2.style.display = "flex";
+        
+        botonCopiar.classList.remove("hidden");
+        document.querySelector(".texto-copiado").style.display ="none";
+
+    } else {
+        right1.style.display = "flex";
+        right2.style.display = "none";
+    }
+    
+}
+
+function btn_copiar() {
+    let textoCopiado = mensaje.value;
+    navigator.clipboard.writeText(textoCopiado);
+    botonCopiar.classList.add("hidden");
+    document.querySelector(".texto-copiado").style.display ="flex";
+    
+
 }
 
 
